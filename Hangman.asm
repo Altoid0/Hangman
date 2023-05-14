4db86d35ae2c4020fbe7f4f651355eafc6c5d063
Tanay Shah <tanay.v.shah@gmail.com>
2023-05-13 22:03:22 -0700

    initialization of guess_word to underscores

--- Hangman.asm
+++ Hangman.asm
@@ -17,6 +17,9 @@
 # $t3: guesses
 # $t4: loop counter for guesses
 # $t5: loop counter for secret_word
+# $t6: secret_word char
+# $t7: guess_char
+# $t8: guess
 
 
 
@@ -30,6 +33,7 @@ incorrect_guess:	.asciiz "\nSorry that's not a letter in the word"
 guess_mesage:		.asciiz "\nGuess a letter: "
 lose_message:		.asciiz "\nSorry you lost, the word was: "
 win_message: 		.asciiz "\nCongratulations, You win!"
+underscore_char: 	.asciiz "_"
 secret_word: 		.space 20
 guess_word:			.space 20
 guess_char: 		.space 1
@@ -71,19 +75,17 @@ subi $t1,$t1,1
 
 
 .macro eval_guess()
-	# Load the user's guessed char into $t8
+	# Load the user's guessed char into $t7
 	la $s2, guess_char 
-	lb $t8, 0($s2)
+	lb $t7, 0($s2)
 	# S0: secret_word
 	# s1: guess_word
 	secret_word_loop:
 		# Load char from secret_word
 		lb $t6, ($s0)
-		# Load char from guess_word (should not be needed)
-		#lb $t7, ($s1)
 		
 		# if guess_char equals secret_word char jump to store_char
-		beq $t6, $t8, store_char
+		beq $t6, $t7, store_char
 		
 		# continue with loop after either storing char or not
 		eval_guess_continue:
@@ -91,18 +93,31 @@ subi $t1,$t1,1
 		addi $s1, $s1, 1	# increment guess_word pointer in parallel
 		addi $t5, $t5, 1	# increment loop counter
 		
-		beq $t5, $t1, secret_word_loop_done
+		beq $t5, $t1, secret_word_loop_end
 		j secret_word_loop
 	
 	store_char:
 		# Store the byte from $t8 (guess_char) into the buffer for guess_word
-		sb $t8, 0($s1)
+		sb $t7, 0($s1)
 		j eval_guess_continue
 		
-	secret_word_loop_done:
+	secret_word_loop_end:
 	# Do nothing, just here to skip past store_char
 .end_macro
 
+
+.macro init_guess_word()
+	la $s3, underscore_char
+	lb $t8, 0($s3)
+	move $t9, $zero
+	guess_word_loop:
+		sb $t8, 0($s3)
+		addi $t9, $t9, 1
+		beq $t9, $t1, guess_word_loop_end
+		addi $s3, $s3, 1
+		j guess_word_loop
+	guess_word_loop_end:
+.end_macro
 
 .text
 main:
@@ -138,6 +153,9 @@ main:
 		li $v0, 1
 		la $a0, ($t1)
 		syscall
+
+		# Initialize guess_word to underscores
+		init_guess_word()
 		
 		beq $t0,1,hard_guesses
 		beq $t0,2,med_guesses
@@ -229,9 +247,9 @@ main:
 		
 		evaluate_guess()
 			
+		beq # if the two strings are equal then branch to win
 		addi $t4, $t4, 1
 		beq $t4, $t3, lose
-		beq # if the two strings are equal then branch to win
 		j check_word
 	lose:
 		print_user_string("\nSorry you lost! Hangman was hung.")
